6b79d193a0d03222cd2974844c2fb6dada60ac1f
codn0185 <codn0185@gmail.com>
2026-07-12 11:01:28 +0900

    feat: Character 추상 클래스 추가
    플레이어와 몬스터가 상속할 Character 추상 클래스 작성

--- cpp-text-rpg/Character.cpp
+++ cpp-text-rpg/Character.cpp
@@ -1,1 +1,63 @@
 #include "Character.h"
+
+void Character::takeDamage(int damage)
+{
+	hp = hp < damage ? 0 : hp - damage;
+}
+
+bool Character::isDead()
+{
+	return hp <= 0;
+}
+
+// Getters
+string Character::getName()
+{
+	return name;
+}
+
+int Character::getHP()
+{
+	return hp;
+}
+
+int Character::getMP()
+{
+	return mp;
+}
+
+int Character::getPower()
+{
+	return power;
+}
+
+int Character::getDefence()
+{
+	return defence;
+}
+
+// Setters
+void Character::setName(string name)
+{
+	this->name = name;
+}
+
+void Character::setHP(int hp)
+{
+	this->hp = hp;
+}
+
+void Character::setMP(int mp)
+{
+	this->mp = mp;
+}
+
+void Character::setPower(int power)
+{
+	this->power = power;
+}
+
+void Character::setDefence(int defence)
+{
+	this->defence = defence
+}
--- cpp-text-rpg/Character.h
+++ cpp-text-rpg/Character.h
@@ -1,5 +1,54 @@
 #pragma once
+
+#include <string>
+
+using namespace std;
+
+// 플레이어 및 몬스터의 기본이 되는 추상 클래스
 class Character
 {
-};
+protected:
+	string name;
 
+	// 	int defaultHP;
+	// 	int maxHP;
+	// 	int currentHP;
+	int hp;
+
+	// 	int defalutMP;
+	// 	int maxMP;
+	// 	int currentMP;
+	int mp;
+
+	// 	int defaultPower;
+	// 	int currentPower;
+	int power;
+
+	// 	int defaultDefence;
+	// 	int currentDefence;
+	int defence;
+
+public:
+	virtual ~Character() = default;
+
+	virtual void takeDamage(int damage) = 0;
+	virtual bool isDead() = 0;
+
+	// Life Cycles
+	//  virtual void onEnable() = 0;
+	//  virtual void onDisable() = 0;
+
+	// Getters
+	virtual string getName() = 0;
+	virtual int getHP() = 0;
+	virtual int getMP() = 0;
+	virtual int getPower() = 0;
+	virtual int getDefence() = 0;
+
+	// Setters
+	virtual void setName(string name) = 0;
+	virtual void setHP(int hp) = 0;
+	virtual void setMP(int mp) = 0;
+	virtual void setPower(int power) = 0;
+	virtual void setDefence(int defence) = 0;
+};
