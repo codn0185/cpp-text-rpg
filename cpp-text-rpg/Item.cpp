cf79062a726cc7bb2874ad2d16b42b3dd1cb1abe
codn0185 <codn0185@gmail.com>
2026-07-12 12:08:46 +0900

    feat: Item 구조체
    이름과 가치를 갖는 아이템 구조체 추가

---
+++ cpp-text-rpg/Item.cpp
@@ -0,0 +1,5 @@
+#include "Item.h"
+
+Item::Item(string name, int price) : name(name), price(price)
+{
+}
---
+++ cpp-text-rpg/Item.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+struct Item
+{
+	string name;
+	int price;
+
+	Item(string name, int price);
+};
