c3f2960726878d815cfc11a4b4c7ea2b841b7bcd
codn0185 <codn0185@gmail.com>
2026-07-14 20:25:46 +0900

    feat: 인벤토리 클래스 추가

--- cpp-text-rpg/Inventory.cpp
+++ cpp-text-rpg/Inventory.cpp
@@ -1,1 +1,76 @@
 #include "Inventory.h"
+
+template<typename ItemID_T>
+Inventory<ItemID_T>::Inventory(int maxSlotCount, int maxStackSize)
+	: maxSlotCount(maxSlotCount), maxStackSize(maxStackSize)
+{
+}
+
+template<typename ItemID_T>
+int Inventory<ItemID_T>::getMaxSlotCount()
+{
+	return maxSlotCount;
+}
+
+template<typename ItemID_T>
+int Inventory<ItemID_T>::getMaxStackSize()
+{
+	return maxStackSize;
+}
+
+template<typename ItemID_T>
+void Inventory<ItemID_T>::setMaxSlotCount(int maxSlotCount)
+{
+	this->maxSlotCount = maxSlotCount;
+}
+
+template<typename ItemID_T>
+void Inventory<ItemID_T>::setMaxStackSize(int maxStackSize)
+{
+	this->maxStackSize = maxStackSize;
+}
+
+template<typename ItemID_T>
+int Inventory<ItemID_T>::getUsedSlotCount()
+{
+	int slots = 0;
+	for (const auto& [itemID, count] : inventory)
+	{
+		if (count % maxStackSize)
+		{
+			slots++;
+		}
+		slots += count / maxStackSize;
+	}
+	return slots;
+}
+
+template<typename ItemID_T>
+bool Inventory<ItemID_T>::isFullSlot()
+{
+	return getUsedSlotCount() == maxSlotCount;
+}
+
+template<typename ItemID_T>
+int Inventory<ItemID_T>::addItem(ItemID_T itemID, int amount)
+{
+	// 추가 가능한 아이템 개수
+	int roomAmount = maxStackSize * (maxSlotCount - getUsedSlotCount()); // 빈 슬롯
+	int reminder = inventory[itemID] % maxStackSize; // 최대치가 아닌 슬롯을 차지하는 아이템 크기
+	if (reminder != 0)
+	{
+		roomAmount += maxStackSize - (reminder); // 최대치가 아닌 슬롯
+	}
+	// 아이템 추가 및 반환
+	if (roomAmount >= amount) // 모두 획득
+	{
+		inventory[itemID] += amount;
+		return 0;
+	}
+	else // 일부 획득 (나머지 반환)
+	{
+		inventory[itemID] += roomAmount;
+		return amount - roomAmount;
+	}
+
+}
--- cpp-text-rpg/Inventory.h
+++ cpp-text-rpg/Inventory.h
@@ -1,10 +1,35 @@
 #pragma once
 
-template <typename T>
+#include "Item.h"
+
+#include <map>
+
+using namespace std;
+
+template <typename ItemID_T>
 class Inventory
 {
-private:
+protected:
+	int maxSlotCount; // 인벤토리 슬롯 수
+	int maxStackSize; // 슬롯 당 스택 개수
+
+	map<ItemID_T, int> inventory; // 인벤토리
 
 public:
+	Inventory(int maxSlotCount, int maxStackSize = 1);
+	~Inventory() = default;
+
+	int getMaxSlotCount();
+	int getMaxStackSize();
+
+	void setMaxSlotCount(int maxSlotCount);
+	void setMaxStackSize(int maxStackSize);
+
+	int getUsedSlotCount(); // 사용 중인 슬롯 개수
+	bool isFullSlot(); // 슬롯 가득 찼는지 여부
+	int addItem(ItemID_T itemID, int amount = 1); // 아이템 추가 (추가 못하고 남은 수 반환)
+
+	virtual void displayInventroy() = 0; // 인벤토리 출력
 };
+
 
