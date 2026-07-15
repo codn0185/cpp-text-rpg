0fbcdb0fe67028caf90a4bfb73e3a796c576c087
codn0185 <codn0185@gmail.com>
2026-07-15 11:43:17 +0900

    fix: using namespace std; 를 제거하여 모호성 해결

--- cpp-text-rpg/InputSystem.h
+++ cpp-text-rpg/InputSystem.h
@@ -2,12 +2,10 @@
 
 #include <string>
 
-using namespace std;
-
 class InputSystem
 {
 public:
-	static bool InputString(string& out);
+	static bool InputString(std::string& out);
 	static bool InputInt(int& out);
 	static bool InputInt(int& out, const int min, const int max);
 };
