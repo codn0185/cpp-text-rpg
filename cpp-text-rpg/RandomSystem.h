83e2756ea5f2b1648e62b5ecc082946aacbf1c64
codn0185 <codn0185@gmail.com>
2026-07-15 12:11:09 +0900

    refactor: 인자를 const 참조로 변경

--- cpp-text-rpg/RandomSystem.h
+++ cpp-text-rpg/RandomSystem.h
@@ -9,11 +9,11 @@ public:
 	static int GetRandomInt(int min, int max);
 
 	template <typename T>
-	static T GetRandomByWeight(std::vector<T> array, std::vector<float> weights);
+	static T GetRandomByWeight(const std::vector<T>& array, const std::vector<float>& weights);
 };
 
 template<typename T>
-inline T RandomSystem::GetRandomByWeight(std::vector<T> array, std::vector<float> weights)
+inline T RandomSystem::GetRandomByWeight(const std::vector<T>& array, const std::vector<float>& weights)
 {
 	std::random_device rd;
 	std::mt19937 gen(rd());
