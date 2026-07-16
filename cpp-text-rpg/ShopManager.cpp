82c601ed6effc0703fea8730a625843271299348
codn0185 <codn0185@gmail.com>
2026-07-16 21:47:02 +0900

    feat: ShopManager 상태 머신 추가
    - onShopPurchase() 메서드 추가

--- cpp-text-rpg/ShopManager.cpp
+++ cpp-text-rpg/ShopManager.cpp
@@ -1,21 +1,134 @@
 #include "ShopManager.h"
 
-ShopDataRow::ShopDataRow(EItemID itemID, int salePrice, int purchasePrice)
-	: itemID(itemID), salePrice(salePrice), purchasePrice(purchasePrice)
+#include "ShopSystem.h"
+#include "InputSystem.h"
+#include "InventorySystem.h"
+
+#include <iostream>
+
+using namespace std;
+
+ShopManager::ShopManager(Player* player, Inventory* backpackInventory, Inventory* stockInventory)
+	: currentShopState(EShopState::SHOP_ENTER), isRunning(true), player(player), backpackInventory(backpackInventory), stockInventory(stockInventory)
 {
 }
 
-const std::unordered_map<EItemID, ShopDataRow> SHOP_TABLE =
+void ShopManager::run()
 {
-	{EItemID::HP_POTION_20, ShopDataRow(EItemID::HP_POTION_20, 100, 200)},
-	{EItemID::MP_POTION_20, ShopDataRow(EItemID::MP_POTION_20, 100, 200)},
+	while (isRunning)
+	{
+		update();
+	}
+}
 
-	{EItemID::BOTTLE, ShopDataRow(EItemID::BOTTLE, 5, 10)},
-	{EItemID::WATER, ShopDataRow(EItemID::WATER, 10, 20)},
-	{EItemID::HEALING_HERB, ShopDataRow(EItemID::HEALING_HERB, 25, 50)},
-	{EItemID::MANA_DUST, ShopDataRow(EItemID::MANA_DUST, 25, 50)},
+void ShopManager::update()
+{
+	switch (currentShopState)
+	{
+	case EShopState::SHOP_ENTER:
+		onShopEnter();
+		break;
+	case EShopState::SHOP_PURCHASE:
+		onShopPurchase();
+		break;
+	case EShopState::SHOP_SALE:
+		onShopSale();
+		break;
+	case EShopState::SHOP_EXIT:
+		onShopExit();
+		break;
+	default:
+		break;
+	}
+}
 
-	{EItemID::SLIME_JELLY, ShopDataRow(EItemID::SLIME_JELLY, 15, 30)},
-	{EItemID::GOBLIN_BLOOD, ShopDataRow(EItemID::GOBLIN_BLOOD, 15, 30)},
-	{EItemID::SKELETON_BONE, ShopDataRow(EItemID::SKELETON_BONE, 15, 30)},
-};
+void ShopManager::switchShopState(EShopState newState)
+{
+	currentShopState = newState;
+}
+
+void ShopManager::onShopEnter()
+{
+	cout << "=============== < 상점 > ===============" << "\n";
+	cout << "1. 구매" << "\n";
+	cout << "2. 판매" << "\n";
+	cout << "3. 인벤토리" << "\n";
+	cout << "0. 메인 메뉴" << "\n";
+	cout << "========================================" << "\n";
+
+	int option = InputSystem::InputIntUnitlValid(0, 3, "선택: ", "* 잘못된 입력입니다.\n");
+	switch (option)
+	{
+	case 0:
+		switchShopState(EShopState::SHOP_EXIT);
+		break;
+	case 1:
+		switchShopState(EShopState::SHOP_PURCHASE);
+		break;
+	case 2:
+		switchShopState(EShopState::SHOP_SALE);
+		break;
+	case 3:
+		InventorySystem::DisplayInventory(backpackInventory, "창고");
+		InventorySystem::DisplayInventory(stockInventory, "배낭");
+		break;
+	default:
+		break;
+	}
+}
+
+void ShopManager::onShopPurchase()
+{
+	vector<EItemID> itemIDs;
+	for (const auto& [itemID, shopDataRow] : SHOP_TABLE)
+	{
+		itemIDs.push_back(itemID);
+	}
+
+	cout << "=============== < 구매 상점 > ===============" << "\n";
+	for (int i = 0; i < itemIDs.size(); i++)
+	{
+		EItemID itemID = itemIDs[i];
+		string itemName = ITEM_TABLE.at(itemID)->name;
+		int purchasePrice = SHOP_TABLE.at(itemID).purchasePrice;
+		cout << i + 1 << ". " << itemName << " ── (" << purchasePrice << "g)" << "\n";
+	}
+	cout << "============================================" << "\n";
+
+	// 구매할 아이템 선택
+	int itemNum = InputSystem::InputIntUnitlValid(0, itemIDs.size(), "아이템 구매 (0: 뒤로가기): ", "* 잘못된 입력입니다.\n");
+	if (itemNum == 0) // 뒤로가기
+	{
+		switchShopState(EShopState::SHOP_ENTER);
+		return;
+	}
+
+	// 구매할 개수 선택
+	const EItemID purchaseItemID = itemIDs[itemNum - 1]; // 구매할 아이템 ID
+	const string purchaseItemName = ITEM_TABLE.at(purchaseItemID)->name; // 구매할 아이템 이름
+	int purchaseCount = InputSystem::InputIntUnitlValid(0, INT_MAX, "구매 개수: ", "* 잘못된 입력입니다.\n"); // 구매할 아이템 개수
+	if (purchaseCount == 0) // 0개 구매 - 구매 목록 다시 보여주기
+	{
+		return;
+	}
+
+	// 구매 시도 - 이후 구매 목록 다시 보여주기
+	bool success = ShopSystem::PruchaseItem(player, backpackInventory, purchaseItemID, purchaseCount);
+	if (!success) // 구매 실패
+	{
+		cout << " > \"" << purchaseItemName << "\" " << purchaseCount << "개 구매에 실패하였습니다." << "\n";
+	}
+	else // 구매 성공
+	{
+		cout << " > \"" << purchaseItemName << "\" " << purchaseCount << "개 구매 성공!" << "\n";
+	}
+}
+
+void ShopManager::onShopSale()
+{
+}
+
+void ShopManager::onShopExit()
+{
+	isRunning = false;
+}
--- cpp-text-rpg/ShopManager.h
+++ cpp-text-rpg/ShopManager.h
@@ -1,28 +1,39 @@
 #pragma once
 
 #include "Item.h"
+#include "Player.h"
+#include "Inventory.h"
 
 #include <unordered_map>
 
-struct ShopDataRow
-{
-	EItemID itemID;
-	int salePrice; // 판매 가격
-	int purchasePrice; // 구매 가격
-
-	ShopDataRow(EItemID itemID, int salePrice, int purchasePrice);
-};
-
-extern const std::unordered_map<EItemID, ShopDataRow> SHOP_TABLE;
-
 enum class EShopState
 {
-	SHOP_INIT, // 상점 진입 - 선택에 따라 다음 상태 이동
-
-
+	SHOP_ENTER, // 상점 진입 - 선택에 따라 다음 상태 이동
+	SHOP_PURCHASE, // 구매 상점 - 아이템 구매
+	SHOP_SALE, // 판매 상점 - 아이템 판매
+	SHOP_EXIT, // 상점 나가기 - 현재 상태 머신 종료
 };
 
 class ShopManager
 {
+private:
+	EShopState currentShopState;
+	bool isRunning;
+
+	Player* player;
+	Inventory* backpackInventory;
+	Inventory* stockInventory;
+
+public:
+	ShopManager(Player* player, Inventory* backpackInventory, Inventory* stockInventory);
+
+	void run();
+	void update();
+	void switchShopState(EShopState newState);
+
+	void onShopEnter();
+	void onShopPurchase();
+	void onShopSale();
+	void onShopExit();
 };
 
