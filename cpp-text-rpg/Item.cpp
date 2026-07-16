3e288d110dcc63b69a2eb0dd742a0f6d1249ee8c
codn0185 <codn0185@gmail.com>
2026-07-16 10:10:32 +0900

    feat: 포션 레시피 수정 (BOTTLE 추가, 재료 양 변경)

--- cpp-text-rpg/Item.cpp
+++ cpp-text-rpg/Item.cpp
@@ -7,8 +7,9 @@ using namespace std;
 
 const map<EItemID, shared_ptr<Item>> ITEM_TABLE = {
 	// 포션
-	{EItemID::HP_POTION_20, make_shared<Item>(Potion(EItemID::HP_POTION_20, EPotionType::HP_POTION, "HP 포션 (소형)", "HP를 20만큼 회복한다.", 30, 20, {{EItemID::WATER, 1}, {EItemID::HEALING_HERB, 2}}))},
-	{EItemID::MP_POTION_20, make_shared<Item>(Potion(EItemID::MP_POTION_20, EPotionType::MP_POTION, "MP 포션 (소형)", "MP를 20만큼 회복한다.", 30, 20, {{EItemID::WATER, 1}, {EItemID::MANA_DUST, 2}}))},
+	{EItemID::HP_POTION_20, make_shared<Item>(Potion(EItemID::HP_POTION_20, EPotionType::HP_POTION, "HP 포션 (소형)", "HP를 20만큼 회복한다.", 30, 20, {{EItemID::BOTTLE, 1}, {EItemID::WATER, 2}, {EItemID::HEALING_HERB, 3}}))},
+	{EItemID::MP_POTION_20, make_shared<Item>(Potion(EItemID::MP_POTION_20, EPotionType::MP_POTION, "MP 포션 (소형)", "MP를 20만큼 회복한다.", 30, 20, {{EItemID::BOTTLE, 1}, {EItemID::WATER, 2}, {EItemID::MANA_DUST, 3}}))},
+
 	// 재료
 	{EItemID::BOTTLE, make_shared<Item>(Ingredient(EItemID::BOTTLE, "공병", "", 3))},
 	{EItemID::WATER, make_shared<Item>(Ingredient(EItemID::WATER, "물", "", 5))},
@@ -18,6 +19,7 @@ const map<EItemID, shared_ptr<Item>> ITEM_TABLE = {
 	{EItemID::SLIME_JELLY, make_shared<Item>(Ingredient(EItemID::SLIME_JELLY, "슬라임의 끈적한 젤리", "", 15))},
 	{EItemID::GOBLIN_BLOOD, make_shared<Item>(Ingredient(EItemID::GOBLIN_BLOOD, "고블린의 오염된 피", "", 15))},
 	{EItemID::SKELETON_BONE, make_shared<Item>(Ingredient(EItemID::SKELETON_BONE, "스켈레톤의 부서진 뼈", "", 15))},
+
 };
 
 Item::Item(EItemID itemID, EItemType itemType, string name, string description, int price)
