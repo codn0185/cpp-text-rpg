0154e6cdb0436158abf9620e732c3b982ad51c13
codn0185 <codn0185@gmail.com>
2026-07-20 17:30:42 +0900

    refactor: 메서드 인자 추가 - 입력 메시지, 에러 메시지

--- cpp-text-rpg/InputSystem.cpp
+++ cpp-text-rpg/InputSystem.cpp
@@ -4,26 +4,30 @@
 
 using namespace std;
 
-bool InputSystem::InputString(string& out)
+bool InputSystem::InputString(string& out, string inputMessage, string errorMessage)
 {
+	cout << inputMessage;
 	if (!getline(cin, out))
 	{
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		out.clear();
+		cout << errorMessage;
 		return false;
 	}
 
 	return true;
 }
 
-bool InputSystem::InputInt(int& out, const int min, const int max)
+bool InputSystem::InputInt(int& out, const int min, const int max, string inputMessage, string errorMessage)
 {
+	cout << inputMessage;
 	if (!(cin >> out))
 	{
 		cin.clear();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		out = 0;
+		cout << errorMessage;
 		return false;
 	}
 
--- cpp-text-rpg/InputSystem.h
+++ cpp-text-rpg/InputSystem.h
@@ -5,8 +5,8 @@
 class InputSystem
 {
 public:
-	static bool InputString(std::string& out);
-	static bool InputInt(int& out, const int min = INT_MIN, const int max = INT_MAX);
+	static bool InputString(std::string& out, std::string inputMessage = "", std::string errorMessage = "");
+	static bool InputInt(int& out, const int min = INT_MIN, const int max = INT_MAX, std::string inputMessage = "", std::string errorMessage = "");
 
 	static int InputIntUnitlValid(const int min = INT_MIN, const int max = INT_MAX, std::string inputMessage = "", std::string errorMessage = "");
 };
