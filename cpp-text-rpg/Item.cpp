6945f09c56838ef60e64cfe2e4299a909ed766ff
codn0185 <codn0185@gmail.com>
2026-07-27 10:47:44 +0900

    feat: 아이템 종류 추가

--- cpp-text-rpg/Item.cpp
+++ cpp-text-rpg/Item.cpp
@@ -17,8 +17,18 @@ const map<EItemID, shared_ptr<Item>> ITEM_TABLE = {
 	{EItemID::MANA_DUST, make_shared<Item>(Item(EItemID::MANA_DUST, EItemType::INGREDIENT, "마나 가루", ""))},
 	// 기타
 	{EItemID::SLIME_JELLY, make_shared<Item>(Item(EItemID::SLIME_JELLY, EItemType::MISC, "슬라임의 끈적한 젤리", ""))},
-	{EItemID::GOBLIN_BLOOD, make_shared<Item>(Item(EItemID::GOBLIN_BLOOD, EItemType::MISC, "고블린의 오염된 피", ""))},
-	{EItemID::SKELETON_BONE, make_shared<Item>(Item(EItemID::SKELETON_BONE, EItemType::MISC, "스켈레톤의 부서진 뼈", ""))},
+	{EItemID::GOBLIN_DAGGER, make_shared<Item>(Item(EItemID::GOBLIN_DAGGER, EItemType::MISC, "고블린의 조잡한 단검", ""))},
+	{EItemID::BAT_FANG, make_shared<Item>(Item(EItemID::BAT_FANG, EItemType::MISC, "박쥐의 날카로운 이빨", ""))},
+
+	{EItemID::ZOMBIE_LEATHER, make_shared<Item>(Item(EItemID::ZOMBIE_LEATHER, EItemType::MISC, "좀비의 부패한 가죽", ""))},
+	{EItemID::SKELETON_BONE, make_shared<Item>(Item(EItemID::SKELETON_BONE, EItemType::MISC, "스켈레톤의 단단한 뼛조각", ""))},
+	{EItemID::GHOST_ESSENCE, make_shared<Item>(Item(EItemID::GHOST_ESSENCE, EItemType::MISC, "고스트의 원혼 정수", ""))},
+
+	{EItemID::ORK_TENDON, make_shared<Item>(Item(EItemID::ORK_TENDON, EItemType::MISC, "오크의 질긴 힘줄", ""))},
+	{EItemID::LIZARDMAN_SCALE, make_shared<Item>(Item(EItemID::LIZARDMAN_SCALE, EItemType::MISC, "리자드맨의 붉은 비늘", ""))},
+	{EItemID::GARGOYLE_SHARD, make_shared<Item>(Item(EItemID::GARGOYLE_SHARD, EItemType::MISC, "가고일의 석상 파편", ""))},
+
+	{EItemID::DRAGON_CORE, make_shared<Item>(Item(EItemID::DRAGON_CORE, EItemType::MISC, "드래곤 코어", ""))},
 
 };
 
--- cpp-text-rpg/Item.h
+++ cpp-text-rpg/Item.h
@@ -12,15 +12,24 @@ enum EItemID
 	// 포션
 	HP_POTION_20,
 	MP_POTION_20,
+
 	// 재료
 	BOTTLE, // 공병 (포션 재료 및 포션 사용 시 자동 반환)
-	WATER,
-	HEALING_HERB,
-	MANA_DUST,
+	WATER, // 물
+	HEALING_HERB, // 치료 허브
+	MANA_DUST, // 마나 가루
+
 	// 기타
-	SLIME_JELLY,
-	GOBLIN_BLOOD,
-	SKELETON_BONE,
+	SLIME_JELLY, // 슬라임 젤리
+	GOBLIN_DAGGER, // 고블린 피
+	BAT_FANG, // 박쥐 이빨
+	ZOMBIE_LEATHER, // 좀비 가죽
+	SKELETON_BONE, // 스켈레톤 뼈
+	GHOST_ESSENCE, // 고스트 정수
+	ORK_TENDON, // 오크 힘줄
+	LIZARDMAN_SCALE, // 리자드맨 비늘
+	GARGOYLE_SHARD, // 가고일 파편
+	DRAGON_CORE, // 드래곤 정수
 };
 
 enum EItemType
