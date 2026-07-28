bbda97253a14ae378b3a3ae4457f12fba88e4fa7
codn0185 <codn0185@gmail.com>
2026-07-28 10:36:22 +0900

    refactor: 미사용 코드 제거

--- cpp-text-rpg/cpp-text-rpg.cpp
+++ cpp-text-rpg/cpp-text-rpg.cpp
@@ -1,11 +1,8 @@
 ﻿#include "GameManager.h"
-#include "GameStateMachine.h"
 
 int main()
 {
 	GameManager& gm = GameManager::GetInstance();
 	gm.run();
 
-	// 	GameStateMachine game;
-	// 	game.run();
 }
